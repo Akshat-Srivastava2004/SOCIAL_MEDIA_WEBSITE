0058e68743f9a7cbb10ed5a29a46cff0ac628cb6
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2024-04-22 20:08:45 +0530

    ss

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")                           // path where all files will stored 
+      cb(null, "https://social-media-website-gw12.onrender.com/./public/temp")                           // path where all files will stored 
     },
     filename: function (req, file, cb) {
       
