cc19bc213f30f208e8a3807e4427176fb8f73960
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2024-04-27 21:06:42 +0530

    logoutadded

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,9 +16,13 @@ import { allcomment } from "../controller/admin.controller.js";
 import { allreply } from "../controller/admin.controller.js";
 import { deleteusercomment } from "../controller/admin.controller.js";
 import { deleteuserreply } from "../controller/admin.controller.js";
+import { checkauthorization } from "../middlewares/authentication.middleware.js";
+import { checkadminlogin} from "../middlewares/authentication.middleware.js";
 import { loginuserdetail } from "../controller/follow.controller.js";
 import { followfunction } from "../controller/follow.controller.js";
 import { followerdetails } from "../controller/follow.controller.js";
+import { checklogout } from "../middlewares/authentication.middleware.js";
+import { userlogout } from "../controller/logout.controller.js";
 const router=Router()
 
 router.route("/register").post(
@@ -40,30 +44,30 @@ router.route("/Postuser").post(
     ]),
     Postuser
 )
-router.route("/:postId/like").post(likepostbyuser)
+router.route("/:postId/like").post(checkauthorization,likepostbyuser)
 
-router.route("/delete/:id").delete(deletepost)
-router.route("/image").get(getAllImages)
-router.route("/detailuser/:id").get(userdetail)
-router.route("/allpost").get(allimages)
-router.route("/usernamedetail/:id").get(userfirstname)
-router.route("/useridd").get(useridentity)
-router.route("/postdetailcomment/:id").get(postauthordetail)
-router.route("/commentbyuser").post(Commentbyloginuser)
-router.route("/replybyuser").post(replydatabase)
+router.route("/delete/:id").delete(checkauthorization,deletepost)
+router.route("/image").get(checkauthorization,getAllImages)
+router.route("/detailuser/:id").get(checkauthorization,userdetail)
+router.route("/allpost").get(checkauthorization,allimages)
+router.route("/usernamedetail/:id").get(checkauthorization,userfirstname)
+router.route("/useridd").get(checkauthorization,useridentity)
+router.route("/postdetailcomment/:id").get(checkauthorization,postauthordetail)
+router.route("/commentbyuser").post(checkauthorization,Commentbyloginuser)
+router.route("/replybyuser").post(checkauthorization,replydatabase)
 router.route("/adminlogin").post(adminlogin)
-router.route("/allpostadmin").get(allpost)
-router.route("/alluserdetailforadmin").get(Allregisteruserdetail)
+router.route("/allpostadmin").get(checkadminlogin,allpost)
+router.route("/alluserdetailforadmin").get(checkadminlogin,Allregisteruserdetail)
 router.route("/userdeletepermanent/:id").delete(deleteuserdetail)
 router.route("/userpostdeleted/:id").delete(deleteuserpost)
-router.route("/allcommentforadmin").get(allcomment)
+router.route("/allcommentforadmin").get(checkadminlogin,allcomment)
 router.route("/allreplyforadmin").get(allreply)
 router.route("/deleteusercomment/:id").delete(deleteusercomment)
 router.route("/deleteuserreply/:id").delete(deleteuserreply)
 router.route("/loginuserdetail").get(loginuserdetail)
 router.route("/follow/:followerId/:followingId").post(followfunction)
 router.route("/followerdetail").get(followerdetails)
-
+router.route("/logout").get(checklogout,userlogout)
 export default router
 
 
